1aa506a2d3c9c9e44ff9651a421fd59c67ec70fa
tripleCC <triplec.linux@gmail.com>
2017-11-06 19:05:56 +0800

    feat: 添加有理数校验

---
+++ TBVListAdapterManager/Validator/TBVListFigureValidator.h
@@ -0,0 +1,13 @@
+//
+//  TBVListFigureValidator.h
+//  TBVListAdapterManager
+//
+//  Created by tripleCC on 2017/11/4.
+//
+
+#import "TBVListValidator.h"
+
+NS_SWIFT_NAME(ListFigureValidator)
+@interface TBVListFigureValidator : TBVListValidator
+
+@end
