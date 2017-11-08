e77611b35086c62518cf97528f7dfdc21b8631cf
tripleCC <triplec.linux@gmail.com>
2017-11-08 09:18:08 +0800

    fix: trim figure

--- TBVListAdapterManager/Validator/TBVListFigureValidator.m
+++ TBVListAdapterManager/Validator/TBVListFigureValidator.m
@@ -9,7 +9,7 @@
 
 @implementation TBVListFigureValidator
 - (NSError *)validateValue:(id)value name:(NSString *)name {
-    if ([value isKindOfClass:[NSString class]] && [value length] > 0 && ![[NSPredicate predicateWithFormat:@"SELF MATCHES %@", @"^[0-9]*(\\.[0-9]*)?$"] evaluateWithObject:value]) {
+    if ([value isKindOfClass:[NSString class]] && [value length] > 0 && ![[NSPredicate predicateWithFormat:@"SELF MATCHES %@", @"^[0-9]*(\\.[0-9]*)?$"] evaluateWithObject:[value stringByTrimmingCharactersInSet:NSCharacterSet.whitespaceAndNewlineCharacterSet]]) {
         return [NSError tbv_errorWithMessage:[NSString stringWithFormat:@"%@格式不正确!", name] validator:self];
     }
     return nil;
